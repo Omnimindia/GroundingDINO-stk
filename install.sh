d967d0595f27cfd134758ff59739f0ef3ca61056
fireblue95 <kaoyi19999@gmail.com>
2023-07-21 16:43:19 +0800

    Update

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+# This method involves using pip3 to install the package directly.
+# Replace `<torch-version>` and `<cuda-version>` with the versions
+# you wish to install. The `cuda-version` does not include a `dot`.
+
+# <torch-version> = 2.0
+# <cuda-version> = 117
+# Example:
+# pip3 install groundingdino-stk==1.0.<torch-version>.<cuda-version> --extra-index-url https://download.pytorch.org/whl/cu<cuda-version>
+
+
 export TORCH_VERSION=2.0
 export CUDA_VERSION=117
 
